8304285db7fe737bcfd73969fdcbe8f42bc9bba8
BerFlores <berflores19@gmail.com>
2021-02-17 15:56:28 -0600

    v1

--- Plataforma/config.py
+++ Plataforma/config.py
@@ -5,7 +5,6 @@ SECRET_KEY = '\xf0?a\x9a\\\xff\xd4;\x0c\xcbHi'
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + r"\uploads"
 
-DEBUG = True
-# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@127.0.0.1:3306/plataforma"
+DEBUG = False
 SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
