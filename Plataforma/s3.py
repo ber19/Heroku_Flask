7f4174da08dfb2dfd171b0881032c9f272e2193a
BerFlores <berflores19@gmail.com>
2021-02-18 19:43:14 -0600

    descarga

--- Plataforma/s3.py
+++ Plataforma/s3.py
@@ -1,5 +1,4 @@
 import boto3
-from botocore.exceptions import ClientError
 from decouple import config
 
 def upload_file(file1, object_name):
@@ -7,8 +6,8 @@ def upload_file(file1, object_name):
     s3 = boto3.client('s3')
     s3.upload_fileobj(file1, bucket, object_name)
 
-# def download_file(object_name):
-#     bucket = config('S3_BUCKET')
-#     file_name = object_name
-#     s3 = boto3.client('s3')
-#     s3.download_file(bucket, object_name, file_name)
+def download_file(object_name):
+    bucket = config('S3_BUCKET')
+    file_name = object_name
+    s3 = boto3.client('s3')
+    s3.download_file(bucket, object_name, file_name)
